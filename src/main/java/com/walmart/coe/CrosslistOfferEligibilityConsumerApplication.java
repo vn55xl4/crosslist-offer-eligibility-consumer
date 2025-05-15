f83e5f8b75590a6e209cc2fdfefd696edb6197e1
vn55xl4 <ajay.kandregula@walmart.com>
2025-05-14 17:40:20 -0700

    creating a repository for consumer"

---
+++ src/main/java/com/walmart/coe/CrosslistOfferEligibilityConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.walmart.coe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrosslistOfferEligibilityConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrosslistOfferEligibilityConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/walmart/coe/CrosslistOfferEligibilityConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walmart.coe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrosslistOfferEligibilityConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
